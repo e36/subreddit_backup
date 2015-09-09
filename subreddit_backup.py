d040c0a29cee6ce07c79a4f4f5d54b758a298aeb
e36 <agent4159@gmail.com>
2015-09-09 17:14:15 -0500

    Initial Commit

---
+++ subreddit_backup.py
@@ -0,0 +1,86 @@
+__author__ = 'e36'
+
+import praw
+import json
+
+print('***Subreddit Backup***')
+
+# TODO: Create a new method of inputting post IDs.  I'd like to be able to specify a list of IDs.
+thread_id = '33qfpf'    # we're just going to use this one for now, so I don't have to keep tying it in
+
+# init praw stuff
+r = praw.Reddit(user_agent='praw test /u/e36')
+
+# get submission
+thread = r.get_submission(submission_id=thread_id)
+
+# get top comments
+# this will look at all parent comments and get their score, and read off the top 10
+
+# init containers where we will hold the thread info
+thread_list = dict()
+comment_list = []
+
+# init the 'already done' set
+already_done = set()
+
+# get comments and flatten
+thread.replace_more_comments(limit=None, threshold=1)
+# comments = thread.comments
+flat_comments = praw.helpers.flatten_tree(thread.comments)
+
+# get thread info and add to thread_list
+print('Getting thread information.')
+thread_data = {
+    'created_utc': thread.created_utc,
+    'id': thread.id,
+    'name': thread.name,
+    'author': thread.author.name,
+    'domain': thread.domain,
+    'link_flair_text': thread.link_flair_text,
+    'num_comments': thread.num_comments,
+    'permalink': thread.permalink,
+    'score': thread.score,
+    'selftext': thread.selftext,
+    'selftext_html': thread.selftext_html,
+    'title': thread.title,
+    'upvote_ratio': thread.upvote_ratio,
+}
+
+# iterate through comments and add them to comment list before adding to thread_list for json serialization
+print('Getting comment data.')
+for comment in flat_comments:
+
+    comment_dict = dict()
+
+    if comment.id not in already_done:
+
+        comment_dict = {
+            'created_utc': comment.created_utc,
+            'body': comment.body,
+            'body_html': comment.body_html,
+            'id': comment.id,
+            'link_id': comment.link_id,
+            'name': comment.name,
+            'parent_id': comment.parent_id,
+            'score': comment.score,
+        }
+
+        # check for author == None for deleted comments
+        if comment.author != None:
+            comment_dict['author'] = comment.author.name
+        else:
+            comment_dict['author'] = 'None'
+
+        already_done.add(comment.id)
+        comment_list.append(comment_dict)
+
+# add it to thread_list
+thread_list['thread_data'] = thread_data
+thread_list['comments'] = comment_list
+
+print(json.dumps(thread_list))
+print('Done.')
+# sort the list
+# newlist = sorted(comment_list, key=lambda k: k['score'], reverse=True)
+
