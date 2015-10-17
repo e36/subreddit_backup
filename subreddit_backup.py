38244442e2da9d11f180124facbf41c8ca17866b
e36 <e36@users.noreply.github.com>
2015-10-17 10:26:35 -0500

    Nuked everything and started building it as a bot.  Also began adding database stuff.

--- subreddit_backup.py
+++ subreddit_backup.py
@@ -2,103 +2,28 @@ __author__ = 'e36'
 __version__ = '0.6'
 
 import praw
-import json
-import argparse
-
-
-# command line arguments!  More functionality can be added later as needed
-parser = argparse.ArgumentParser(description='Backs up subreddit threads')
-parser.add_argument('thread_id', metavar='thread', type=str, help='Unique ID of the thread to be backed up')
-args = parser.parse_args()
-
-# get argument variables - TODO: add error catching to this
-thread_id = args.thread_id
+import time
+import configparser
+from database import connect_to_db
+import sqlalchemy
 
 # print intro
-print('***Subreddit Backup***')
+print('***Subreddit Backup Bot***')
 print('version {0}'.format(__version__))
 
+# get config data and save to global variables
+config = configparser.ConfigParser()
+config.read('config.dat')
+DATABASE = config['database']
+SETTINGS = config['settings']
 
 # init praw stuff
 r = praw.Reddit(user_agent='backup script by /u/e36')
 
-# get submission
-thread = r.get_submission(submission_id=thread_id)
+# connect to the database
+db = connect_to_db(DATABASE)
 
-# get top comments
-# this will look at all parent comments and get their score, and read off the top 10
-
-# init containers where we will hold the thread info
-thread_list = dict()
-comment_list = []
-
-# init the 'already done' set
-already_done = set()
-
-# status stuff
-print('Getting thread ' + thread_id)
-
-# get comments and flatten
-thread.replace_more_comments(limit=None, threshold=1)
-# comments = thread.comments
-flat_comments = praw.helpers.flatten_tree(thread.comments)
-
-# get thread info and add to thread_list
-print('Getting thread information.')
-thread_data = {
-    'created_utc': thread.created_utc,
-    'id': thread.id,
-    'name': thread.name,
-    'author': thread.author.name,
-    'domain': thread.domain,
-    'link_flair_text': thread.link_flair_text,
-    'num_comments': thread.num_comments,
-    'permalink': thread.permalink,
-    'score': thread.score,
-    'selftext': thread.selftext,
-    'selftext_html': thread.selftext_html,
-    'title': thread.title,
-    'upvote_ratio': thread.upvote_ratio,
-}
-
-# iterate through comments and add them to comment list before adding to thread_list for json serialization
-print('Getting comment data.')
-for comment in flat_comments:
-
-    comment_dict = dict()
-
-    if comment.id not in already_done:
-
-        comment_dict = {
-            'created_utc': comment.created_utc,
-            'body': comment.body,
-            'body_html': comment.body_html,
-            'id': comment.id,
-            'link_id': comment.link_id,
-            'name': comment.name,
-            'parent_id': comment.parent_id,
-            'score': comment.score,
-        }
-
-        # check for author == None for deleted comments
-        if comment.author != None:
-            comment_dict['author'] = comment.author.name
-        else:
-            comment_dict['author'] = 'None'
-
-        already_done.add(comment.id)
-        comment_list.append(comment_dict)
-
-# add it to thread_list
-thread_list['thread_data'] = thread_data
-thread_list['comments'] = comment_list
-
-# create file name from thread data, and save the json dump
-filename = thread_data['author'] + '-' + thread_data['id'] + '.json'
-
-f = open(filename, 'w')
-f.write(json.dumps(thread_list))
-f.close()
-
-print('Done.')
-
+# main loop
+while True:
+    print("bot")
+    time.sleep(5)
